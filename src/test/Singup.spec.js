28c2612c63cc1c2bbad4135c9b61e0233b6c93bd
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2022-08-19 20:58:46 -0300

    test: ensure SignUpService return 400 if no password is provided

--- src/test/Singup.spec.js
+++ src/test/Singup.spec.js
@@ -4,6 +4,7 @@ import SignUpService from '../app/services/Signup'
 describe('SignUp Service', () => {
   it('Should return 400 if no name is provided', async () => {
     const httpRequest = {
+      name: '',
       email: 'any_email',
       password: 'any_password',
       passwordConfirmation: 'any_password'
@@ -16,7 +17,20 @@ describe('SignUp Service', () => {
   it('Should return 400 if no email is provided', async () => {
     const httpRequest = {
       name: 'any_name',
+      email: '',
       password: 'any_password',
+      passwordConfirmation: 'any_password'
+    }
+    const httpResponse = await SignUpService(httpRequest)
+    console.log(httpResponse)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  it('Should return 400 if no password is provided', async () => {
+    const httpRequest = {
+      name: 'any_name',
+      email: 'any_email',
+      password: '',
       passwordConfirmation: 'any_password'
     }
     const httpResponse = await SignUpService(httpRequest)
