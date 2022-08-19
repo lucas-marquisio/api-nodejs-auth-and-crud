b0eb107aedbe44fd28e8590862afd8079526f343
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2022-08-19 20:44:12 -0300

    test: ensure SignUpService return 400 if no name is provided

--- src/test/Singup.spec.js
+++ src/test/Singup.spec.js
@@ -1,3 +1,15 @@
-describe('', () => {
-  
+/* eslint-disable no-undef */
+import SignUpService from '../app/services/Signup'
+
+describe('SignUp Service', () => {
+  it('Should return 400 if no name is provided', async () => {
+    const httpRequest = {
+      email: 'any_email',
+      password: 'any_password',
+      passwordConfirmation: 'any_password'
+    }
+    const httpResponse = await SignUpService(httpRequest)
+    console.log(httpResponse)
+    expect(httpResponse.statusCode).toBe(400)
+  })
 })
