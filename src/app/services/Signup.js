a1355c20a016c35044e82367df3073ace88303f5
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2022-08-19 21:09:24 -0300

    chore: add SingupService comparation password to passwordConfirm

--- src/app/services/Signup.js
+++ src/app/services/Signup.js
@@ -1,10 +1,13 @@
 import BadRequest from '../utils/errors/BadRequest'
 
 async function SignUpService (SingUpData) {
-  const { name, email, password } = SingUpData
+  const { name, email, password, passwordConfirmation } = SingUpData
+  const passwordMatch = password === passwordConfirmation
+
   if (!name) return BadRequest('Missing param: name')
   if (!email) return BadRequest('Missing param: email')
   if (!password) return BadRequest('Missing param: password')
+  if (!passwordMatch) return BadRequest('password not equal passwordConfirmation')
 
   return {
     statusCode: 200,
