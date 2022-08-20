2588a727f821b67f791dc3353c6617abe799de83
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2022-08-20 17:56:11 -0300

    chore: add SignInController

---
+++ src/app/controllers/signin.js
@@ -0,0 +1,14 @@
+import SignInService from '../services/Signin'
+
+async function SignInController (req, res) {
+  try {
+    const userData = req.body
+    const response = await SignInService(userData)
+    const { statusCode, body } = response
+    return res.status(statusCode).json(body)
+  } catch (error) {
+    return res.status(500).json({ error: 'internal error.' })
+  }
+}
+
+export default SignInController
