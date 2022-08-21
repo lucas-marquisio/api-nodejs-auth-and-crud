6c29784015ac1b7c57900a98b50cb8db9c789869
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2022-08-21 01:43:27 -0300

    chore: add route signin

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import SignUpController from '../controllers/signup.js'
+import SingInController from '../controllers/signin.js'
 
 const route = Router()
 
 route.post('/signup', SignUpController)
+route.post('/signin', SingInController)
 
 export default route
