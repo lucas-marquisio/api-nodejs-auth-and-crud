ed901a803dd6b24a8ad0b1144c0832a83533e3e9
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2022-08-21 01:18:49 -0300

    chore: configurated express router

--- src/app/server.js
+++ src/app/server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import routes from './routes'
 
 const server = express()
 
 dotenv.config()
 server.use(express.json())
 server.use(cors())
+server.use(routes)
 
 export default server
