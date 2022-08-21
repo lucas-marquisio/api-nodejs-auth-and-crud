b846ed4bcbbcf13f57037cdd8b2a6ddf5fa5ad0d
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2022-08-21 01:31:03 -0300

    chore: add route signup

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,5 +1,8 @@
 import { Router } from 'express'
+import SignUpController from '../controllers/signup.js'
 
 const route = Router()
+
+route.post('/signup', SignUpController)
 
 export default route
